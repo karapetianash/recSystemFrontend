19e77d5060cde427918383f7b3c06faff1993af6
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-06-15 21:58:31 +0300

    complete the API

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	r := gin.Default()
+	db, err := sql.Open("sqlite3", "../data/recommendations.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r.GET("/recommendations/:userId/:n", func(c *gin.Context) {
+		getRecommendationsHandler(c, db)
+	})
+
+	r.Run(":8080")
+}
